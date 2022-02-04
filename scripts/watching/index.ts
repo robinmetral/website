f99e14c16328506ab2721bfe158a2cadd5dbac27
Robin Métral <robin.metral@sumup.com>
2022-02-04 17:24:50 +0100

    First d3 'viz'

--- scripts/watching/index.ts
+++ scripts/watching/index.ts
@@ -1,1 +1,8 @@
-console.log("Runtime watching script incoming");
+let movies = ["Power of the Dog", "Azor"];
+
+// we don't import d3 because it's imported as a third-party script on the page
+d3.select("#viz-root")
+  .selectAll("p")
+  .data(movies)
+  .join("p")
+  .text((d) => d);
