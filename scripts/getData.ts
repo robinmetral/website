0c99263833509eded108c9ebd1674488e85e213b
Robin Métral <robin.metral@sumup.com>
2022-02-21 12:03:08 +0100

    Fix path

--- scripts/getData.ts
+++ scripts/getData.ts
@@ -1,17 +1,13 @@
 import { writeFile } from "fs/promises";
 import fetch from "node-fetch";
 
-const PUBLIC_DIR = "./public/";
+const PUBLIC_DIR = "./public";
 
 export default async function getData() {
   try {
     const response = await fetch("https://logger.robinmetral.workers.dev/");
     const json = (await response.json()) as Record<string, string | number>[];
-    await writeFile(
-      `${PUBLIC_DIR}data/films.json`,
-      JSON.stringify(json),
-      "utf8"
-    );
+    await writeFile(`${PUBLIC_DIR}/films.json`, JSON.stringify(json), "utf8");
     console.log("Pulled data from GitHub.");
   } catch (error) {
     console.log(error);
