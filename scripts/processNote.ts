4395690e548e839ddb8c4b69cb98f79d4eb6e67b
Robin Métral <robin.metral@sumup.com>
2022-04-15 16:56:27 +0200

    Display webmention replies on notes

--- scripts/processNote.ts
+++ scripts/processNote.ts
@@ -1,26 +1,94 @@
-/**
- * Finds the note's publishDate and formats it
- */
-function formatDate(html: string, isoDate: string): string {
-  const formattedDate = new Date(isoDate).toLocaleDateString("en-US", {
+import fetch from "node-fetch";
+
+function formatDate(date: string): string {
+  return new Date(date).toLocaleDateString("en-US", {
     weekday: "long",
     year: "numeric",
     month: "long",
     day: "numeric",
   });
-  /**
-   * We match an iso string between the >< characters to avoid transforming
-   * the iso string passed as `datetime` to the `time` element.
-   */
-  return html.replace(/>\d{4}-\d{2}-\d{2}</, `>${formattedDate}<`);
+}
+
+/**
+ * Type adapted from https://webmention.io/api/mentions.jf2?target=https://robinmetral.com/
+ */
+type Comment = {
+  type: "entry";
+  author: {
+    type: "card";
+    name: string;
+    photo: string;
+    url: string;
+  };
+  url: string;
+  published: string;
+  "wm-received": string;
+  "wm-id": number;
+  "wm-source": string;
+  "wm-target": string;
+  name: string;
+  content: {
+    html: string;
+    text: string;
+  };
+  "in-reply-to": string;
+  "wm-property": "in-reply-to";
+  "wm-private": false;
+};
+
+async function fetchComments(path: string) {
+  const url = `https://robinmetral.com${path}`;
+  const res = await fetch(
+    `https://webmention.io/api/mentions.jf2?target=${url}&wm-property=in-reply-to`
+  );
+  if (!res.ok) {
+    throw new Error(`Couldn't fetch webmentions for ${path}`);
+  }
+  const data = (await res.json()) as {
+    type: "feed";
+    name: "Webmentions";
+    children: Comment[];
+  };
+  return data.children;
 }
 
 export async function buildPage(
   html: string,
-  frontmatter: { [key: string]: string }
+  frontmatter: { [key: string]: string },
+  path: string
 ): Promise<string> {
   try {
-    return formatDate(html, frontmatter.publishDate);
+    /**
+     * Format date.
+     * We match an iso string between the >< characters to avoid transforming
+     * the iso string passed as `datetime` to the `time` element.
+     */
+    html = html.replace(
+      />\d{4}-\d{2}-\d{2}</,
+      `>${formatDate(frontmatter.publishDate)}<`
+    );
+    /**
+     * fetch webmentions
+     */
+    const comments = await fetchComments(path);
+    if (comments.length) {
+      html = html.replace(
+        "__COMMENTS__",
+        `<ul>${comments
+          .map(
+            (c) =>
+              `<li><p><a href="${c.author.url}">${
+                c.author.name
+              }</a> replied on ${formatDate(c.published)} (<a href="${
+                c.url
+              }">permalink</a>)</p><p>${c.content.text}</p></li>`
+          )
+          .join("")}</ul>`
+      );
+    } else {
+      html = html.replace("__COMMENTS__", `<p>No comments yet.</p>`);
+    }
+    return html;
   } catch (error) {
     throw new Error(`Failed to build page: ${error}`);
   }
