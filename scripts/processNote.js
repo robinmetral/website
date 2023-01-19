a8cb46030234cd60b38b9a72cda22fe18f664c8b
Robin Métral <robin@metral.ch>
2023-01-19 10:06:13 +0100

    Render category links on a note header
    The note header in the note template (page.html) is extended with categories if frontmatter for that note specifies them. Mustache doesn't support looping through arrays of strings, so to avoid having to add categories to frontmatter as an array of objects, the processNote build script uses regex to transform the categories string into links to category pages.

--- scripts/processNote.js
+++ scripts/processNote.js
@@ -9,15 +9,29 @@ function formatDate(date) {
 
 export async function buildPage(html, frontmatter) {
   try {
-    /**
-     * Format date.
-     * We match an iso string between the >< characters to avoid transforming
-     * the iso string passed as `datetime` to the `time` element.
-     */
-    html = html.replace(
-      />\d{4}-\d{2}-\d{2}</,
-      `>${formatDate(frontmatter.publishDate)}<`
-    );
+    html = html
+      /**
+       * Format date.
+       * We match an iso string between the >< characters to avoid transforming
+       * the iso string passed as `datetime` to the `time` element.
+       */
+      .replace(
+        />\d{4}-\d{2}-\d{2}</,
+        `>${formatDate(frontmatter.publishDate)}<`
+      )
+      /**
+       * Format note categories.
+       * We match categories by the [[CATEGORIES]] substring in the html, and
+       * replace them by links to the proper categories pages.
+       */
+      .replace(/\[\[CATEGORIES(.+)\]\]/, (match, p1) =>
+        p1
+          .split(",")
+          .map(
+            (category) => `<a href="/categories/${category}">${category}</a>`
+          )
+          .join(", ")
+      );
     return html;
   } catch (error) {
     throw new Error(`Failed to build page: ${error}`);
