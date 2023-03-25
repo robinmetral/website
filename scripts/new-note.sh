2387da304111af0ec7d94a4a0f37b0f9395219f7
Robin Métral <robin@metral.ch>
2023-03-25 15:21:46 +0100

    Add bash script to generate new note template

---
+++ scripts/new-note.sh
@@ -0,0 +1,22 @@
+# `iconv -t ascii//TRANSLIT`: transforms to ascii, e.g. `ç` to `c` or `é` to `'e`
+# `sed -E -e 's/expression/replacement/flags'`: substitute in string using sed regexp. `-e` for each new expression
+# - `s/[^[:alnum:]]/-/g`: replace non-alphanumeric characters by a dash
+# - `s/^-+|-+$//g`: remove leading and trailing dashes
+# - `s/-{2,}/-/g`: replace 2+ dashes by a single dash
+# `tr '[:upper:]' '[:lower:]'`: lowercase using tr
+
+# FIXME: salé gets transformed into sal'e which gets transformed into sal-e
+
+slugify () {
+  echo "$1" | iconv -t ascii//TRANSLIT | sed -E -e 's/[^[:alnum:]]/-/g' -e 's/^-+|-+$//g' -e 's/-{2,}/-/g' | tr '[:upper:]' '[:lower:]'
+}
+
+if [ -z "$1" ]
+  then
+    echo "Name the post using 'npm run new \"New post name\"'"
+    exit 1
+else
+  slug=$(slugify "$1")
+  echo -e "---\ntitle: \"$1\" \ncategories:\n  - name: \npublishDate: \"$(date '+%Y-%m-%d')\"\ntemplate: \"/templates/page.html\"\nbuildScript: \"/scripts/processNote.js\"\n---" > pages/notes/$slug.md
+  echo "Created new note \"$1\" at \"/pages/notes/$slug.md\""
+fi
